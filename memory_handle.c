8d02b3bf2b4137170b96d7c6f80b090be09a5316
Sakhile <smkhamz@hotmail.com>
2023-05-22 07:20:05 -0700

    Simple Shell

---
+++ memory_handle.c
@@ -0,0 +1,89 @@
+#include "shell.h"
+
+/**
+ * _memcpy - Copies memory.
+ * @newptr: newly created pointer.
+ * @ptr: initial  pointer.
+ * @size: size of the new pointer.
+ *
+ * Return: nothing.
+ */
+void _memcpy(void *newptr, const void *ptr, unsigned int size)
+{
+	char *char_ptr = (char *)ptr;
+	char *char_newptr = (char *)newptr;
+	unsigned int i;
+
+	for (i = 0; i < size; i++)
+		char_newptr[i] = char_ptr[i];
+}
+
+/**
+ * _realloc - reallocates a memory block.
+ * @ptr: pointer to the memory.
+ * @old_size: size of the allocated space of ptr.
+ * @new_size: new size of the new memory block.
+ *
+ * Return: pointer
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	void *newptr;
+
+	if (ptr == NULL)
+		return (malloc(new_size));
+
+	if (new_size == 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	if (new_size == old_size)
+		return (ptr);
+
+	newptr = malloc(new_size);
+	if (newptr == NULL)
+		return (NULL);
+
+	if (new_size < old_size)
+		_memcpy(newptr, ptr, new_size);
+	else
+		_memcpy(newptr, ptr, old_size);
+
+	free(ptr);
+	return (newptr);
+}
+
+/**
+ * _reallocdp - re-allocates a memory block of a double pointer.
+ * @ptr: double pointer to the memory allocated.
+ * @old_size: size of the allocated space
+ * @new_size: new size of the new memory block.
+ *
+ * Return: pointer
+ * if new_size == old_size, returns pointer.
+ * if malloc fails, returns NULL.
+ */
+char **_reallocdp(char **ptr, unsigned int old_size, unsigned int new_size)
+{
+	char **newptr;
+	unsigned int i;
+
+	if (ptr == NULL)
+		return (malloc(sizeof(char *) * new_size));
+
+	if (new_size == old_size)
+		return (ptr);
+
+	newptr = malloc(sizeof(char *) * new_size);
+	if (newptr == NULL)
+		return (NULL);
+
+	for (i = 0; i < old_size; i++)
+		newptr[i] = ptr[i];
+
+	free(ptr);
+
+	return (newptr);
+}
