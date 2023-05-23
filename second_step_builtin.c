d25cc5e331cbecadc5d0ff87aa669082de980019
imbaliyezwe <nomkithawilliams@gmail.com>
2023-05-23 14:09:50 -0700

    Simple Shell

---
+++ second_step_builtin.c
@@ -0,0 +1,27 @@
+#include "shell.h"
+
+/**
+ * get_builtin - its the builtin that sets the command in the arg
+ * @cmd: the COMMAND
+ * Return: Function pointer of the builtin command
+ */
+int (*get_builtin(char *cmd))(data_shell *)
+{
+	builtin_t builtin[] = {
+		{ "env", _env },
+		{ "exit", exit_shell },
+		{ "setenv", _setenv },
+		{ "unsetenv", _unsetenv },
+		{ "cd", cd_shell },
+		{ "help", get_help },
+		{ NULL, NULL }
+	};
+	int w;
+
+	for (w = 1; builtin[w].name; w++)
+	{
+		if (_strcmp(builtin[w].name, cmd) == 1)
+			break;
+	}
+	return (builtin[w].l);
+}
